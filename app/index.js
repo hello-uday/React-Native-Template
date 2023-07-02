28719c9901d6d7758d0b92a456003c2114c9412b
Uday Pratap Singh <udaypratapsingh95802@gmail.com>
2023-07-02 12:52:02 +0530

    initial changes to template

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { View, ScrollView,SafeAreaView } from 'react-native';
-import {Stack, UseRouter } from 'expo-router';
+import {Stack, useRouter } from 'expo-router';
 
 import { COLORS, icons,images,SIZES } from '../constants';
 import {
